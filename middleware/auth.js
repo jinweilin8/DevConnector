88245a2adb8016a0dcb768d83c2bb1f878dcd9bd
Jinwei Lin <jinwei.lin8@gmail.com>
2020-04-21 17:33:12 -0400

    implement jwt and set up auth route

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = (req, res, next) => {
+  // get token from header
+  const token = req.header('x-auth-token');
+
+  // check if it has token
+  if (!token) {
+    // 401 code for unauthorized
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+
+  // verify token
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
