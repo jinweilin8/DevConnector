2fd1849a5781fd02f6c762e7f8a2e069862e562c
Jinwei Lin <jinwei.lin8@gmail.com>
2020-05-07 23:35:49 -0400

    set up protected route for dashboard

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -22,7 +22,7 @@ const Login = ({ login, isAuthenticated }) => {
 
   // Redirect if logged in
   if (isAuthenticated) {
-    return <Redirect to='/dashboad' />;
+    return <Redirect to='/dashboard' />;
   }
 
   return (
