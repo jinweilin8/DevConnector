296f7c15b507b910d917995e5f5fceca3beb0257
Jinwei Lin <jinwei.lin8@gmail.com>
2020-05-13 11:54:34 -0400

    add comments, display them and delete

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -4,7 +4,9 @@ import {
   POST_ERROR,
   UPDATE_LIKES,
   ADD_POST,
-  GET_POST
+  GET_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT,
 } from '../actions/types';
 
 const initialState = {
@@ -28,8 +30,8 @@ export default function (state = initialState, action) {
       return {
         ...state,
         post: payload,
-        loading: false
-      }
+        loading: false,
+      };
     case ADD_POST:
       return {
         ...state,
@@ -56,7 +58,26 @@ export default function (state = initialState, action) {
         ),
         loading: false,
       };
-
+    case ADD_COMMENT:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          comments: payload,
+        },
+        loading: false,
+      };
+    case REMOVE_COMMENT:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          comments: state.post.comments.filter(
+            (comment) => comment._id !== payload
+          ),
+        },
+        loading: false,
+      };
     default:
       return state;
   }
